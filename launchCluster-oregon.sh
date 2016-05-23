cfcef6dd3ad3d5110f2e0f7e5829734a879d60c6
Bill McGair <bill@mcgair.com>
2016-05-22 18:02:10 -0700

    HackOregonpstack up for 2016

--- launchCluster-oregon.sh
+++ launchCluster-oregon.sh
@@ -4,7 +4,7 @@
 #### Increment BASTION_ID to launch new box!  ####
 ### Otherwise script will behave idempotently ####
 ##################################################
-export BASTION_IDEMPOTENT_ID="01"
+export BASTION_IDEMPOTENT_ID="10"
 
 export AWS_REGION="us-west-2"
 export ZONEA="us-west-2a"
@@ -12,7 +12,7 @@ export ZONEB="us-west-2b"
 export ZONEC="us-west-2c"
 
 export VPC_BASE="10.40"
-export KEYPAIR="BillM-staging"
+export KEYPAIR="HackOregon16"
 export MGMT_IP="`curl -s icanhazip.com`/32"
 
 /usr/local/bin/ansible-playbook ./stage1.yml
